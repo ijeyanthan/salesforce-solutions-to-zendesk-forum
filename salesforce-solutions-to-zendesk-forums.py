24282725f85cd377051e2882b8214deaa96a7a8b
Jeyanthan <jeyanthan@collab.net>
2013-07-05 18:49:35 +0530

    initial commit

---
+++ salesforce-solutions-to-zendesk-forums.py
@@ -0,0 +1,21 @@
+import json
+import urllib2
+import csv
+import base64
+import os
+import glob
+
+os.chdir(".")
+username = 'jeyanthan@collab.net'
+password = 'Collabnet1!'
+
+inf = csv.reader(open('svn solutions.csv','r'))
+for row in inf:
+	print row[0]
+	content = row[1].replace('\n','<br/>')
+	data = {"topic": {"forum_id": 22263091u, "title": row[0], "body": content, "topic_type": "articles", "access": "logged in users" } }
+	jsondata = json.dumps(data)
+	req = urllib2.Request(url='https://svndocs.zendesk.com/api/v2/topics.json', data = jsondata, headers={'Content-Type': 'application/json'})
+	base64string = base64.encodestring('%s:%s' % (username,password)).replace('\n','')
+	req.add_header("Authorization","Basic %s" % base64string)
+	result = urllib2.urlopen(req)
