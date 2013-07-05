a11b8e31eedecd77022417e461059ce8818d7973
Jeyanthan I <ijeyanthan@gmail.com>
2013-07-05 23:22:53 +0530

    Update salesforce-solutions-to-zendesk-forums.py
    updated the URL

--- salesforce-solutions-to-zendesk-forums.py
+++ salesforce-solutions-to-zendesk-forums.py
@@ -6,16 +6,16 @@ import os
 import glob
 
 os.chdir(".")
-username = 'jeyanthan@collab.net'
-password = 'Collabnet1!'
+username = 'yourname@company.com'
+password = 'yourpassword!'
 
-inf = csv.reader(open('svn solutions.csv','r'))
+inf = csv.reader(open('salesforce solutions.csv','r'))
 for row in inf:
 	print row[0]
 	content = row[1].replace('\n','<br/>')
 	data = {"topic": {"forum_id": 22263091u, "title": row[0], "body": content, "topic_type": "articles", "access": "logged in users" } }
 	jsondata = json.dumps(data)
-	req = urllib2.Request(url='https://svndocs.zendesk.com/api/v2/topics.json', data = jsondata, headers={'Content-Type': 'application/json'})
+	req = urllib2.Request(url='https://yoursite.zendesk.com/api/v2/topics.json', data = jsondata, headers={'Content-Type': 'application/json'})
 	base64string = base64.encodestring('%s:%s' % (username,password)).replace('\n','')
 	req.add_header("Authorization","Basic %s" % base64string)
 	result = urllib2.urlopen(req)
