a8e73ac4548a69fb908a6fbc65d069cefdeb6d08
Jeyanthan I <ijeyanthan@gmail.com>
2014-05-13 10:12:13 +0530

    Update salesforce-solutions-to-zendesk-forums.py
    Fixed a typo

--- salesforce-solutions-to-zendesk-forums.py
+++ salesforce-solutions-to-zendesk-forums.py
@@ -13,7 +13,7 @@ inf = csv.reader(open('salesforce solutions.csv','r'))
 for row in inf:
 	print row[0]
 	content = row[1].replace('\n','<br/>')
-	data = {"topic": {"forum_id": 22263091u, "title": row[0], "body": content, "topic_type": "articles", "access": "logged in users" } }
+	data = {"topic": {"forum_id": 22263091, "title": row[0], "body": content, "topic_type": "articles", "access": "logged in users" } }
 	jsondata = json.dumps(data)
 	req = urllib2.Request(url='https://yoursite.zendesk.com/api/v2/topics.json', data = jsondata, headers={'Content-Type': 'application/json'})
 	base64string = base64.encodestring('%s:%s' % (username,password)).replace('\n','')
